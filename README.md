8579be5d17bd5e7451edecb034eeae03d073e02e
Edward J. Stronge <ejstronge@gmail.com>
2014-07-14 20:36:13 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,17 @@
+
+# Nickleby.js
+
+# Edward J. Stronge <ejstronge@gmail.com>
+
+An (adorably-named) interface to the NCBI web services.
+
+The first goal is to support retrieving data from Pubmed - other databases
+will be added over time.
+
+# Installation
+
+
+# Usage
+
+
+(c) 2014, Edward J. Stronge. Released under the GPLv3 - see LICENSE.
---
+++ nickleby.js
@@ -0,0 +1,24 @@
+//     nickleby.js
+//     (c) 2014 Edward J. Stronge
+//     nickleby.js is distributed under the GPLv3 - see LICENSE
+
+(function() {
+  var Nickleby = (function () {
+
+    var Nickleby = function(options) {
+      void 0;
+    };
+
+    ncbi = {};
+
+    return ncbi;
+  }());
+
+
+  if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
+    module.exports = nickleby;
+  } else {
+    window.nickleby = nickleby;
+  }
+
+}());
