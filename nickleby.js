05f613b8e22ee084be76225babd3b263ce77dc23
Edward J. Stronge <ejstronge@gmail.com>
2015-01-12 14:45:48 -0500

    Fix two syntax bugs
    This commit fixes an incorrect comparison between `undefined` and
    a function that should have returned the value `undefined`. This
    comparison would always fail as the function's return value is represented
    as 'undefined', a string.
    
    Also, replaced a comma with a semicolon.

--- nickleby.js
+++ nickleby.js
@@ -7,10 +7,10 @@ module.exports = (function() {
   var jquery = require('jquery'),
       // Load jsdom if this script is running in Node
       $ = jquery(
-        typeof window === undefined ? window : require('jsdom').jsdom().parentWindow),
+        typeof window === 'undefined' ? window : require('jsdom').jsdom().parentWindow),
       toolname = 'nickleby.js',
       author_email = 'nickleby.js@gmail.com',
-      last_request = 0,
+      last_request = 0;
 
   ////////////////////////////////////////////////////////////////////////
   // Searching
